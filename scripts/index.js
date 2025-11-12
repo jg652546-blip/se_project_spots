3f7f5565975ff092638e014f6de7c358cf3ab07e
Jose Goncalves <Jose.Goncalves@gmail.com>
2025-11-12 14:57:59 -0500

    commit message

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+const editProfilebtn = document.QuerySelector(".profile__edit-btn");
+const editProfileModal = document.QuerySelector("pedit-profile-modal");
+const editProfileClosebtn = editProfileModal.QuerySelecto(".modal__close-btn");
