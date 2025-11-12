80e2c10a2d012d3999c84c9a75713877f338c739
Jose Goncalves <Jose.Goncalves@gmail.com>
2025-11-12 16:44:14 -0500

    commit message

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
-const editProfilebtn = document.QuerySelector(".profile__edit-btn");
-const editProfileModal = document.QuerySelector("pedit-profile-modal");
-const editProfileClosebtn = editProfileModal.QuerySelecto(".modal__close-btn");
+const editProfilebtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileClosebtn = editProfileModal.querySelector(".modal__close-btn");
+
+const newPostbtn = document.querySelector(".profile__new-post-btn");
+const newPostModal = document.querySelector("#edit-post-modal");
+const newPostClosebtn = newPostModal.querySelector(".modal__close-btn");
+
+editProfilebtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileClosebtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
